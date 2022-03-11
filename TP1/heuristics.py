40c1850f84e88fcaaab859114b0fd2d9c62206b4
Juan Quintairos <jquintairos@itba.edu.ar>
2022-03-11 19:33:17 -0300

    Agregada heurística distancia manhattan

--- TP1/heuristics.py
+++ TP1/heuristics.py
@@ -1,16 +1,31 @@
 from models import State
 from constants import *
 
+number_goal_positions = {}      # not hardcoded in case we want to extend for nxm boards
+n = 1
+for i in range(0, HEIGHT):
+    for j in range(0, WIDTH):
+        number_goal_positions[n] = (i, j)
+        n += 1
+del(number_goal_positions[HEIGHT*WIDTH])
+
 def total_squares(state:State):
-    squares = WIDTH*HEIGHT
+    squares = WIDTH*HEIGHT - 1  # -1 because the blank space is not counted
 
     for (row_index, row) in enumerate(state.board):
         for (col_index,square_val) in enumerate(row):
             if(((square_val-1) // HEIGHT) == row_index and ((square_val-1) % WIDTH) == col_index ): #comparison to identify if the element is placed in the correct position
                 squares-=1
-            elif(square_val == 0 and row_index == WIDTH-1 and col_index == HEIGHT-1): #position of empty square should be in the last square of the board by finishing the game
-                squares-=1
-    
+
     return squares
 
 
+def total_manhattan(state:State):
+    manhattan_distance = 0
+    for (row_index, row) in enumerate(state.board):
+        for (col_index,square_val) in enumerate(row):
+            if (square_val != 0):
+                (row_goal, col_goal) = number_goal_positions[square_val]
+                manhattan_distance += abs(row_index - row_goal) + abs(col_index - col_goal)
+    
+    return manhattan_distance
--- TP1/main.py
+++ TP1/main.py
@@ -2,23 +2,23 @@ from models import *
 from heuristics import *
 from algorithm_chooser import execute_algorithm
 
-initial_state = State([[1,8,3],[4,5,2],[0,7,6]], [1,0])
-initial_state.heuristic = total_squares(initial_state)
+initial_state = State([[1,2,3],[5,6,4],[0,7,8]], [2,0])
+initial_state.heuristic = total_manhattan(initial_state)
 #initial_state = State([[1,2,3],[4,5,6],[0,7,8]], [2,0], 0)
 root = Node(initial_state,None,0)
 objective_state = State([[1,2,3],[4,5,6],[7,8,0]], [2,2])
 
 #BPP
-metrics = execute_algorithm("BPP", root, objective_state, None)
+#metrics = execute_algorithm("BPP", root, objective_state, None)
 
 #HEUR_GLOBAL
-#metrics = execute_algorithm("HEUR_GLOBAL", root, objective_state, total_squares)
+#metrics = execute_algorithm("HEUR_GLOBAL", root, objective_state, total_manhattan)
 
 #HEUR_LOCAL   with backtracking
-#metrics = execute_algorithm("HEUR_LOCAL", root, objective_state, total_squares)
+#metrics = execute_algorithm("HEUR_LOCAL", root, objective_state, total_manhattan)
 
 #A*
-#metrics = execute_algorithm("A*", root, objective_state, total_squares)
+metrics = execute_algorithm("A*", root, objective_state, total_manhattan)
 
 print("Status: {0}".format("success" if metrics.solved else "failure"))
 print("Depth: {0}".format(metrics.depth))
