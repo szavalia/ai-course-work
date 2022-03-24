779fc0a7a586063232fdc1424ae8c09e3fed65d7
Juan Quintairos <jquintairos@itba.edu.ar>
2022-03-24 20:58:49 -0300

    Roulette selection added

--- TP2/selection.py
+++ TP2/selection.py
@@ -7,6 +7,37 @@ def elite_selection(population):
     del population[(len(population)//2):]
     return population
 
+def roulette_selection(population):
+    #Probability of being selected is proportional to fitness
+    new_pop = []
+
+    #Get total fitness
+    total_fitness = 0
+    for individual in population:
+        total_fitness += individual.fitness
+
+    #Fill probabilities
+    relative_fitness = []
+    probabilities_array = []
+    population.sort(key=lambda individual: individual.fitness, reverse=True) #One full sort so as to lower the overall passes over probabilities array (the first ones are the most likely to be chosen if this is done)
+    for index, individual in enumerate(population):
+        relative_fitness.append(individual.fitness/total_fitness)
+        probabilities_array.append(sum(relative_fitness[:index+1]))
+
+    #Draw winners
+    for i in range(len(population)//2):
+        rand = random.random()
+        for index, probability in enumerate(probabilities_array):
+            if(rand < probability):
+                new_pop.append(population[index])
+                break
+
+    return new_pop
+
+def rank_selection(population):
+    #Probability of being selected is proportional to relative fitness
+    return population
+
 def tournament_selection(population):
     #Take a pair at random and make them fight for a spot, with the fittest having more chance of success
     random.shuffle(population)
@@ -34,6 +65,10 @@ def selection_chooser(selection):
     tournament_threshold = selection["tournament_threshold"]
     if(method == "elite"):
         return Selection(method, elite_selection)
+    if(method == "roulette"):
+        return Selection(method, roulette_selection)
+    if(method == "rank"):
+        return Selection(method, rank_selection)
     if(method == "tournament"):
         Selection.tournament_threshold = tournament_threshold
         return Selection(method, tournament_selection)
