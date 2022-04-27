a50d3efef93f763f9e258f97ce9226febc47d826
JSicardi <jsicardi@itba.edu.ar>
2022-04-27 17:59:40 -0300

    Multilayer added to main

--- TP3/main.py
+++ TP3/main.py
@@ -1,13 +1,17 @@
 from io_parser import parse_properties,generate_output
 from models import Properties,Observables
-from algorithms.perceptron import execute
+from algorithms.perceptron import execute as simple_execute
+from algorithms.multilayer import execute as multi_execute
 def __main__():
 
     #Parse parameters
     properties:Properties = parse_properties()
 
     #Execute the algorithm based on the properties
-    metrics:Observables = execute(properties)
+    if(properties.perceptron.type == "multilayer"):
+        metrics:Observables = multi_execute(properties)
+    else:
+        metrics:Observables = simple_execute(properties)
 
     #Process metrics for data visualization
     generate_output(properties,metrics)
