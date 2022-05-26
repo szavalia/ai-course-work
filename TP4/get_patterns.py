67bb2583b43ffb3c7c0281c73b8ae2bcf4d68237
JSicardi <jsicardi@itba.edu.ar>
2022-05-26 15:08:32 -0300

    Combinations printed added as parameter

--- TP4/get_patterns.py
+++ TP4/get_patterns.py
@@ -1,6 +1,9 @@
 from letters import alphabet
 import itertools
 import numpy as np
+import sys
+
+max_combins = int(sys.argv[1])
 
 flat_letters = {
     k: np.array(m).flatten() for (k, m) in alphabet.items()
@@ -22,6 +25,5 @@ for g in all_groups:
 
 avg_dot_product = sorted(avg_dot_product, key=lambda x: x[0], reverse=False)
 
-size = 5
-for i in range(size):
+for i in range(max_combins):
     print("Patterns: {0}, Avg_dot: {1}".format(avg_dot_product[i][1],avg_dot_product[i][0]))
