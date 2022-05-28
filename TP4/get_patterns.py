30c91da8aa038212c9ef778eac05a60241f0da78
JSicardi <jsicardi@itba.edu.ar>
2022-05-28 16:58:09 -0300

    Added aditional order parameter to get_patterns

--- TP4/get_patterns.py
+++ TP4/get_patterns.py
@@ -4,6 +4,7 @@ import numpy as np
 import sys
 
 max_combins = int(sys.argv[1])
+order = sys.argv[2].lower() == 'true'
 
 flat_letters = {
     k: np.array(m).flatten() for (k, m) in alphabet.items()
@@ -23,7 +24,7 @@ for g in all_groups:
     max_dot_product.append((np.abs(orto_matrix).max(), g))
     index+=1
 
-avg_dot_product = sorted(avg_dot_product, key=lambda x: x[0], reverse=False)
+avg_dot_product = sorted(avg_dot_product, key=lambda x: x[0], reverse=order)
 
 for i in range(max_combins):
     print("Patterns: {0}, Avg_dot: {1}".format(avg_dot_product[i][1],avg_dot_product[i][0]))
